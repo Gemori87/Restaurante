9b362b2c72b6e7748f9bccec2d0b93342dc9cf87
Gemori87 <oscarmotta87@gmail.com>
2025-02-24 09:59:53 +1100

    base de datos

--- sistema/restaurante/models.py
+++ sistema/restaurante/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class restaurante(models.Model):
+    id = models.AutoField(primary_key=True)
+    platillo = models.CharField(max_length=100, verbose_name='Título')
+    imagen = models.ImageField(upload_to='imagenes/',verbose_name="Imagen", null=True)
+    descripcion = models.TextField(verbose_name="Descripción",null=True)
